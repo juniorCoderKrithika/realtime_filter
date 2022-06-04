220be0467bc3d4b71e034891ca9dced2f9236c93
juniorCoderKrithika <72335062+juniorCoderKrithika@users.noreply.github.com>
2022-06-04 15:30:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX="";
+noseY="";
+
 function preload(){
 
 }
@@ -6,12 +9,33 @@ function setup(){
 canvas=createCanvas(300,300);
 canvas.center();
 video=createCapture(VIDEO);
+video.size(300,300);
 video.hide();
+ 
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotPoses);
 }
 
 function draw(){
 image(video,0,0,300,300);
+fill(255,0,0);
+stroke(255,0,0);
+circle(noseX,noseY,20);
 }
 function take_snapshot(){
 save("MyFilterSelfie.png")
+}
+
+function modelLoaded(){
+    console.log("Posenet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+console.log(results);
+console.log("noseX = " + results[0].pose.nose.x);
+console.log("noseY = " + results[0].pose.nose.y);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+    }
 }
