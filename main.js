036fc805fae5164b95d2a40e0c3fe7d801719cd8
juniorCoderKrithika <72335062+juniorCoderKrithika@users.noreply.github.com>
2022-06-07 16:23:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ noseY="";
 
 function preload(){
 
+    clown_nose=loadImage("https://i.postimg.cc/HkgVNM4w/clown-nose.png");
 }
 
 function setup(){
@@ -18,24 +19,22 @@ posenet.on('pose',gotPoses);
 
 function draw(){
 image(video,0,0,300,300);
-fill(255,0,0);
-stroke(255,0,0);
-circle(noseX,noseY,20);
+image(clown_nose,noseX,noseY,30,30);
 }
 function take_snapshot(){
 save("MyFilterSelfie.png")
 }
 
-function modelLoaded(){
-    console.log("Posenet is initialized");
-}
-
-function gotPoses(results){
-    if(results.length > 0){
-console.log(results);
-console.log("noseX = " + results[0].pose.nose.x);
-console.log("noseY = " + results[0].pose.nose.y);
-noseX=results[0].pose.nose.x;
-noseY=results[0].pose.nose.y;
+    function modelLoaded(){
+        console.log("Posenet is initialized");
     }
-}
+
+    function gotPoses(results){
+        if(results.length > 0){
+    console.log(results);
+    console.log("noseX = " + results[0].pose.nose.x);
+    console.log("noseY = " + results[0].pose.nose.y);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+        }
+    }
